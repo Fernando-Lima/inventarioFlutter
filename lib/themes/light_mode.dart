161c0e0de1c76c98f9261ac266d55a4f64149212
Fernando Lima <fernando.lima@eccreguladora.com.br>
2024-04-18 14:55:53 -0300

    explicando como funciona o themeData

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+// first we create a lightMode theme using ThemeData of Material
+// this way we can get this theme calling theme.of(context)
+// but  first it's necessary to call this class in Main.dart at class MyApp in field theme:
+
 ThemeData lighMode = ThemeData(
   colorScheme: ColorScheme.light(
     background: Colors.grey.shade300,
